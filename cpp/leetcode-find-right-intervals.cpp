b5fc015c71a65dd5093dbc0744f48d39bf502770
swapfloor <106497594+swapfloor@users.noreply.github.com>
2023-04-18 23:34:34 +0800

    Create leetcode-find-right-intervals.cpp

---
+++ cpp/leetcode-find-right-intervals.cpp
@@ -0,0 +1,21 @@
+// 二分搜索
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        vector<pair<int,int>>s(n);
+        for(int i=0;i<n;i++){
+            s[i]={intervals[i][0],i};
+        }
+        sort(s.begin(),s.end());
+        vector<int>ans(n,-1);
+        for(int i=0;i<n;i++){
+            int l=intervals[i][0],r=intervals[i][1];
+            auto it=lower_bound(s.begin(),s.end(),pair<int,int>{r,0});
+            if(it!=s.end()){
+                ans[i]=it->second;
+            }
+        }
+        return ans;
+    }
+};

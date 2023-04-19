c98c7c7f1c20b29def5756da1b951ec10fb2d3e8
swapfloor <106497594+swapfloor@users.noreply.github.com>
2023-04-19 11:39:41 +0800

    Create leetcode-jump-game-vii.cpp

---
+++ cpp/leetcode-jump-game-vii.cpp
@@ -0,0 +1,20 @@
+// slide window 
+// 可以想向成一个队列，不在队列就剔除
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        if (s.back() == '1') return false;
+        int n = s.size();
+        vector<bool> reach(n);
+        reach[0] = true;
+        int cur = 0;
+        for (int i = minJump; i < n; i++) {
+            if (i - maxJump - 1 >= 0) {
+                cur -= reach[i - maxJump - 1];
+            }
+            cur += reach[i - minJump];
+            if (cur > 0 && s[i] == '0') reach[i] = true;
+        }
+        return reach[n - 1];
+    }
+};

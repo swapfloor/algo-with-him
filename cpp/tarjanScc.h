fc3a926b31ccc6ac935b4994dd31cc602cd092d2
swapfloor <106497594+swapfloor@users.noreply.github.com>
2023-04-20 19:08:38 +0800

    Update tarjanScc.h

--- cpp/tarjanScc.h
+++ cpp/tarjanScc.h
@@ -2,20 +2,61 @@ namespace treetree{
 struct TarjanScc {
     int n;
     int timeStamp;
+    bool isWork;
     std::vector<std::vector<int>> g;
+    std::vector<std::vector<int>> topoG;
+    std::vector<int> din, dout;
     std::vector<int> dfn, low;
     std::vector<int> stk;
     std::vector<int> inStk;
-    std::vector<int> id, size={0};
+    std::vector<int> id, size;
     int sccCnt;
-    TarjanScc(int n) : n(n), g(n), dfn(n), low(n), inStk(n), id(n), sccCnt(0), timeStamp(0) {}
+    TarjanScc(int n) : n(n), g(n), dfn(n), low(n), inStk(n), id(n), sccCnt(0), timeStamp(0), isWork(false) {}
     void addEdge(int from,int to){
         assert(from>=0&&from<n&&to>=0&&to<n);
         g[from].emplace_back(to);
     }
     void work() {
+        isWork = true;
         for (int i = 0; i < n; i++) {
             if (!dfn[i]) tarjanWork(i);
+        }
+    }
+    void buildTopoDin() {
+        assert(isWork);
+        assert(sccCnt > 0);
+        din = std::vector<int>(sccCnt);
+        for (int u = 0; u < n; u++) {
+            for (auto v : g[u]) {
+                int a = id[u], b = id[v];
+                if (a != b) {
+                    din[b]++;
+                }
+            }
+        }
+    }
+    void buildTopoDout() {
+        assert(isWork);
+        assert(sccCnt > 0);
+        dout = std::vector<int>(sccCnt);
+        for (int u = 0; u < n; u++) {
+            for (auto v : g[u]) {
+                int a = id[u], b = id[v];
+                if (a != b) {
+                    dout[a]++;
+                }
+            }
+        }
+    }
+    void buildTopoGraph() {
+        topoG = std::vector<std::vector<int>>(n);
+        for (int u = 0; u < n; u++) {
+            for (auto v : g[u]) {
+                int a = id[u], b = id[v];
+                if (a != b) {
+                    topoG[a].push_back(b);
+                }
+            }
         }
     }
 private:
@@ -34,7 +75,6 @@ private:
         }
 
         if (dfn[x] == low[x]) {
-            sccCnt++;
             size.push_back(0);
             int y;
             do {
@@ -44,6 +84,7 @@ private:
                 id[y] = sccCnt;
                 size[sccCnt]++;
             } while (y != x);
+            sccCnt++;
         }
     }
 };

88d322fa29a9eb91fadeb07cc812bbbf27427bed
swapfloor <106497594+swapfloor@users.noreply.github.com>
2023-04-22 18:33:21 +0800

    Create cf1337a.cpp

---
+++ cpp/cf1337a.cpp
@@ -0,0 +1,16 @@
+// https://codeforces.com/contest/1337/problem/A
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		int a, b, c, d;
+		cin >> a >> b >> c >> d;
+		cout << a << " " << c << " " << c << "\n";	
+	}
+	return 0; 
+} 

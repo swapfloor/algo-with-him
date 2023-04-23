39b190cc6df2fce0b1e5c8d6c68300c96b45e8e1
swapfloor <106497594+swapfloor@users.noreply.github.com>
2023-04-23 21:35:30 +0800

    Create leetcode-design-linked-list.cpp

---
+++ cpp/leetcode-design-linked-list.cpp
@@ -0,0 +1,77 @@
+// https://leetcode.cn/problems/design-linked-list
+struct Node {
+    int v;
+    Node *l, *r;
+    Node(int v = 0) : v(v), l(nullptr), r(nullptr) {}
+};
+class MyLinkedList {
+public:
+    Node *pl, *pr;
+    int sz;
+    MyLinkedList() {
+        sz = 0;
+        pl = new Node();
+        pr = new Node();
+        pl->r = pr;
+        pr->l = pl;
+    }
+    
+    int get(int index) {
+        if (index < 0 || index >= sz) {
+            return -1;
+        }
+        auto cur = pl;
+        for (int i = 0; i <= index; i++) {
+            cur = cur->r;
+        }
+        return cur->v;
+    }
+    
+    void addAtHead(int val) {
+        addAtIndex(0, val);
+    }
+    
+    void addAtTail(int val) {
+        addAtIndex(sz, val);
+    }
+    
+    void addAtIndex(int index, int val) {
+        if (index > sz) {
+            return;
+        }
+        auto cl = pl;
+        for (int i = 0; i < index; i++) {
+            cl = cl->r;
+        }
+        sz++;
+        auto cr = cl->r;
+        auto node = new Node(val);
+        cr->l = node;
+        node->r = cr;
+        node->l = cl;
+        cl->r = node;
+    }
+    
+    void deleteAtIndex(int index) {
+        if (index >= sz) {
+            return;
+        }
+        auto cur = pl;
+        for (int i = 0; i <= index; i++) {
+            cur = cur->r;
+        }
+        sz--;
+        cur->l->r = cur->r;
+        cur->r->l = cur->l;
+    }
+};
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList* obj = new MyLinkedList();
+ * int param_1 = obj->get(index);
+ * obj->addAtHead(val);
+ * obj->addAtTail(val);
+ * obj->addAtIndex(index,val);
+ * obj->deleteAtIndex(index);
+ */

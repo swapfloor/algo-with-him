52190f4465959a5a3fa568f40ebf188391e42e96
swapfloor <106497594+swapfloor@users.noreply.github.com>
2023-04-29 15:05:27 +0800

    Create init.cpp

---
+++ cpp/init.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+/* output type name */
+#include <cxxabi.h>
+#define output_typename(x) cout<<typeid(x).name()<<" => "<<abi::__cxa_demangle(typeid(x).name(), NULL, NULL, NULL)<<"\n"
+/* */
+
+typedef long long i64;
+using namespace std;
+
+void solve() {
+    
+} 
+
+int main() {
+    solve();
+    return 0;
+}
+// focus implement not algo
+// prepare for implement
+// a lettle : 
+// - a lettle sqrt factor
+// - a lettle map to index
+// - a lettle make pair
+// - a lettle prepare prefix or suffix
+// - a lettle sort or pq
+// - a lettle brute
+// - a lettle comb

5b10c1dd395df12a940af9ac07c04770274340e0
swapfloor <106497594+swapfloor@users.noreply.github.com>
2023-09-10 20:30:50 +0800

    Create lc70.rs

---
+++ Rust/matrix/lc70.rs
@@ -0,0 +1,46 @@
+// https://leetcode.cn/problems/climbing-stairs/description/
+#[derive(Debug, Clone, Copy)]
+// 0 1
+// 2 3
+pub struct Mat {
+    x: (i64, i64, i64, i64)
+}
+
+impl std::ops::Mul for Mat {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self {
+        Mat { x: (
+                self.x.0 * rhs.x.0 + self.x.1 * rhs.x.2,
+                self.x.0 * rhs.x.1 + self.x.1 * rhs.x.3,
+                self.x.2 * rhs.x.0 + self.x.3 * rhs.x.2,
+                self.x.2 * rhs.x.1 + self.x.3 * rhs.x.3
+            )
+        }
+    }
+}
+
+impl Solution {
+    /// ```
+    /// f(x) = f(x - 1) + f(x - 2)
+    /// ```
+    /// ```
+    /// [ f(x + 1) ] = [ 1 1 ] [   f(x)   ]
+    /// [   f(x)   ] = [ 1 0 ] [ f(x - 1) ]
+    /// ```
+    /// ```
+    /// [ f(x + 1) ] = [ 1 1 ] ^ x [ f(1) ]
+    /// [   f(x)   ] = [ 1 0 ]     [ f(0) ]
+    /// ```
+    pub fn climb_stairs(mut n: i32) -> i32 {
+        let mut a = Mat{ x: (1, 1, 1, 0) };
+        let mut r = Mat{ x: (1, 0, 0, 1) };
+        while n > 0 {
+            if n % 2 != 0 {
+                r = r * a;
+            }
+            a = a * a;
+            n /= 2;
+        }
+        (r.x.2 + r.x.3) as i32
+    }
+}
